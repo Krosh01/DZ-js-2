f0990933885c98c0bd162fec9a7791252eaa9ad7
Krosh01 <muhambetovmels@gmail.com>
2022-09-10 16:22:29 +0600

    first triannglet

---
+++ chess.js
@@ -0,0 +1,6 @@
+// var full = ' # # # # ';
+// for (var i = 0; i < 8; i++)
+//    console.log(''+full.substring(i%2,8)+'')
+
+
+   
---
+++ triangle.js
@@ -0,0 +1,7 @@
+let str = "";
+for (let i = 0; i < 8; i++) {
+  console.log(str);
+  str += "*";
+}
+
+
